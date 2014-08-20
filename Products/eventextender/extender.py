810d6373fd4a2599a368ff386a59785eb5cd3a50
Carol McMasters-Stone <cbeck@ucdavis.edu>
2014-08-20 15:05:49 -0700

    additional files and fields

--- Products/eventextender/extender.py
+++ Products/eventextender/extender.py
@@ -1,4 +1,4 @@
-from Products.Archetypes.public import ImageField, ImageWidget, StringField, StringWidget
+from Products.Archetypes.public import ImageField, ImageWidget, StringField, StringWidget, SelectionWidget
 from Products.ATContentTypes.interface import IATEvent
 from Products.Archetypes.atapi import *
 from archetypes.schemaextender.field import ExtensionField
@@ -30,8 +30,17 @@ class eventsImageExtender(object):
                 label=u"Image Title",
                 description=u"Title of image to display with the Event",
             ),
-        ),
-    ]
+         ),
+         _ExtensionStringField(
+             "choosepublic",
+             widget = SelectionWidget(
+                 label=u"Event Audience",
+                 description=u"Choose Public or Private",
+                 format="radio",
+             ),
+             vocabulary=["Public","Private","Other"]
+           ),
+      ]
 
     def __init__(self, context):
         self.context = context
